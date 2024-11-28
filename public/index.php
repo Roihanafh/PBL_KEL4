473bc8edd7cc950a3ccebe6bcaf73a342ffaa13e
Salsabila Mahda Runisha <salsabilamahda217@gmail.com>
2024-11-28 15:13:21 +0700

    navbar landing page

---
+++ public/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistem Informasi Prestasi Mahasiswa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/style2.css">
+</head>
+<body>
+   <!-- Navbar -->
+<div class="navbar">
+    <img src="../assets/logo_polinema.png" alt="Logo Lencana">
+    <div>
+        <a href="#">Beranda</a>
+        <a href="">Telusuri Prestasi</a>
+        <a href="#">Tentang Kami</a>
+        <a href="#">Kontak</a>
+        <a href="#" class="login">Login</a>
+    </div>
+</div>
+
+
+<!-- isi landing page -->
+
+
+</body>
+</html>
