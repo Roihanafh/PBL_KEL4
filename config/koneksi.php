138d7f32192a61e0b83c44d3cf2497243943e8b3
Roihanafh <roihanalfahmi123@gmail.com>
2024-12-02 11:27:48 +0700

    add database dan koneksi

---
+++ config/koneksi.php
@@ -0,0 +1,19 @@
+<?php
+// Konfigurasi koneksi database
+$serverName = "DESKTOP-1B5FK7F"; // Server SQL Server Anda
+$connectionOptions = [
+    "Database" => "PBL_Lencana", // Nama database
+    "Uid" => "",    // Username SQL Server
+    "PWD" => ""     // Password SQL Server
+];
+
+// Membuat koneksi
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+
+// Cek koneksi
+if ($conn === false) {
+    die(print_r(sqlsrv_errors(), true)); // Menampilkan pesan error jika koneksi gagal
+}
+
+echo "Koneksi berhasil ke database PBL_Lencana!";
+?>
