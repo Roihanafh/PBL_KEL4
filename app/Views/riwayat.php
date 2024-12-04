cfd1038d2e609793d28d58decc15147e73f434ea
Thesambala <zilanzalilan18@gmail.com>
2024-12-05 03:38:46 +0700

    Nambah file yang hilang

---
+++ app/Views/riwayat.php
@@ -0,0 +1,31 @@
+<div class="riwayat">
+    <div class="container mt-5">
+        <p>Data Prestasi</p>
+        <hr class="line">
+        <div class="mb-3">
+            <button class="button" onclick="redirectToFormPrestasi()">
+                <i class="fas fa-plus"></i> 
+                Data Baru
+            </button>
+        </div>
+        <div class="table-responsive">
+            <table id="dataPrestasi" class="table table-striped table-bordered">
+                <thead class="table-primary text-center">
+                    <tr>
+                        <th>No</th>
+                        <th>NIM</th>
+                        <th>Nama</th>
+                        <th>Judul Kompetisi</th>
+                        <th>Tingkat</th>
+                        <th>Peringkat</th>
+                        <th>Tahun</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- Data dari database -->
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div
