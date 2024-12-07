e363e75ea0d4f324945227e06dd5281b62c8ae4d
Salsabila Mahda Runisha <salsabilamahda217@gmail.com>
2024-12-07 13:50:01 +0700

    fix

--- public/indexmhs.php
+++ public/indexmhs.php
@@ -2,24 +2,24 @@
 session_start();
 include('../config/koneksi.php');
 
-$page = isset($_GET['page']) ? $_GET['page'] : 'home'; // Default to 'home' if no page is specified
-$nimLogin = $_SESSION['nim'];
+    $page = isset($_GET['page']) ? $_GET['page'] : 'home'; // Default to 'home' if no page is specified
+    $nimLogin = $_SESSION['nim'];
 
-// Mengecek apakah mahasiswa sudah memiliki data prestasi
-$hasPrestasi = false;
+    // Mengecek apakah mahasiswa sudah memiliki data prestasi
+    $hasPrestasi = false;
 
-if ($page === 'prestasi') {
-    $sqlCheck = "SELECT COUNT(*) AS Jumlah FROM PrestasiMahasiswa WHERE Nim = ?";
-    $params = array($nimLogin);
-    $stmt = sqlsrv_query($conn, $sqlCheck, $params);
+    if ($page === 'prestasi') {
+        $sqlCheck = "SELECT COUNT(*) AS Jumlah FROM PrestasiMahasiswa WHERE Nim = ?";
+        $params = array($nimLogin);
+        $stmt = sqlsrv_query($conn, $sqlCheck, $params);
 
-    if ($stmt && $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
-        $hasPrestasi = $row['Jumlah'] > 0; // true jika memiliki data prestasi
+        if ($stmt && $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+            $hasPrestasi = $row['Jumlah'] > 0; // true jika memiliki data prestasi
+        }
+        
+        // Arahkan ke halaman yang sesuai
+        $page = $hasPrestasi ? 'riwayat' : 'prestasi';
     }
-    
-    // Arahkan ke halaman yang sesuai
-    $page = $hasPrestasi ? 'riwayat' : 'prestasi';
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -31,6 +31,21 @@ if ($page === 'prestasi') {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="../assets/style3.css">
+    <script>
+    function toggleSidebar() {
+        const sidebar = document.querySelector('.sidebar');
+        const header = document.querySelector('.header');
+        const container = document.querySelector('.container');
+        const addPrestasi = document.querySelector('.addprestasi'); 
+        
+        sidebar.classList.toggle('responsive');
+        header.classList.toggle('responsive');
+        addPrestasi.classList.toggle('responsive'); 
+    }
+    function redirectToFormPrestasi() {
+    window.location.href = 'indexmhs.php?page=formprestasi';
+}  
+</script>
 </head>
 <body>
     <?php include('../assets/header.php'); ?>
